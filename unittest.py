f869e4cb98f7f83700b3245298cb3b8e037ebf59
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-12-04 00:07:58 +0400

    added a parameterized test for quantile

--- unittest.py
+++ unittest.py
@@ -4,6 +4,20 @@ import code
 # импортируем pytest и mock для тестирования
 import pytest
 import mock
+import numpy as np
+
+# определяем фикстуру для генерации случайных данных
+@pytest.fixture
+def random_data():
+    # задаем параметры распределения
+    mean = 10
+    std = 5
+    size = 100
+    # генерируем два массива случайных чисел из нормального распределения
+    x = np.random.normal(mean, std, size)
+    y = np.random.normal(mean, std, size)
+    # возвращаем кортеж из двух массивов
+    return x, y
 
 # параметризуем тесты для функции mean
 @pytest.mark.parametrize("x, expected", [
@@ -98,3 +112,24 @@ def test_variance_with_mock_mean(mock_mean):
     assert code.variance(np.array([1, 2, 3, 4, 5])) == 0
     # проверяем, что функция mean была вызвана один раз с правильным аргументом
     mock_mean.assert_called_once_with(np.array([1, 2, 3, 4, 5]))
+    
+# определяем параметризованный тест для функции quantile
+@pytest.mark.parametrize("p, expected", [
+    (0, 0), # нулевой квантиль равен минимальному значению
+    (0.5, 10), # медиана равна математическому ожиданию
+    (1, 20), # единичный квантиль равен максимальному значению
+    (-0.1, ValueError), # отрицательный порядок квантиля вызывает исключение
+    (1.1, ValueError) # больше единицы порядок квантиля вызывает исключение
+])
+def test_quantile(random_data, p, expected):
+    # получаем массив случайных данных из фикстуры
+    x, _ = random_data
+    # если ожидаемое значение является исключением
+    if isinstance(expected, Exception):
+        # проверяем, что функция quantile вызывает это исключение
+        with pytest.raises(expected):
+            quantile(x, p)
+    # иначе
+    else:
+        # проверяем, что функция quantile возвращает ожидаемое значение с заданной точностью
+        assert np.isclose(quantile(x, p), expected, atol=1)
