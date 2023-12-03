691b44e1335e6e71d2e61faa8742e93694e18c1f
Pancake2021 <92122200+Pancake2021@users.noreply.github.com>
2023-12-03 22:52:21 +0400

    adding a test for the sdt function

--- unit_test.py
+++ unit_test.py
@@ -26,3 +26,14 @@ def test_mean(x, expected):
 def test_variance(x, expected):
     # проверяем, что функция variance возвращает ожидаемый результат
     assert code.variance(x) == expected
+
+# параметризуем тесты для функции std
+@pytest.mark.parametrize("x, expected", [
+    (np.array([1, 2, 3, 4, 5]), 1.4142135623730951),
+    (np.array([-1, -2, -3, -4, -5]), 1.4142135623730951),
+    (np.array([0]), 0),
+    (np.array([1.5, 2.5, 3.5]), 0.816496580927726)
+])
+def test_std(x, expected):
+    # проверяем, что функция std возвращает ожидаемый результат
+    assert code.std(x) == expected
