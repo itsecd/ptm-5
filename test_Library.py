dc6a25d9ba23296dcc8e0f58a4712f23b6d817e9
Dmitrii14 <117352471+Dmitrii14@users.noreply.github.com>
2023-12-05 16:51:27 +0300

    Написал функции для Юнит-тестов и добавил в них докстринги и тайпхитинги

--- test_Library.py
+++ test_Library.py
@@ -1,1 +1,115 @@
+import logging
 import pytest
+from main import Library, Person, Book
+
+
+@pytest.fixture
+def library():
+    """
+    Aункция для создания экземпляра Library
+    :return: Library
+    """
+    return Library()
+
+
+def test_add_person(library):
+    """
+    Тест для метода add_person
+    :param library: Library
+    :return: None
+    """
+    person = Person("John", 28)
+    library.add_person(person)
+    assert person in library.people
+
+
+def test_remove_person(library):
+    """
+    Тест для метода remove_person
+    :param library: Library
+    :return: None
+    """
+    person = Person("Jane", 35)
+    library.add_person(person)
+    library.remove_person(person)
+    assert person not in library.people
+
+
+def test_add_book(library):
+    """
+    Тест для метода add_book
+    :param library: Library
+    :return: None
+    """
+    book = Book("The Catcher in the Rye", "J.D. Salinger")
+    library.add_book(book)
+    assert book in library.books
+
+
+def test_remove_book(library):
+    """
+    Тест для метода remove_book
+    :param library: Library
+    :return: None
+    """
+    book = Book("1984", "George Orwell")
+    library.add_book(book)
+    library.remove_book(book)
+    assert book not in library.books
+
+
+def test_lend_book(library):
+    """
+    Тест для метода lend_book
+    :param library: Library
+    :return: None
+    """
+    person = Person("Charlie", 40)
+    book = Book("Animal Farm", "George Orwell")
+    library.add_person(person)
+    library.add_book(book)
+    library.lend_book(book, person)
+    assert book.owner == person
+    assert book in person.owned_books
+
+
+@pytest.mark.parametrize("filename", ["test_inventory.txt", "nonexistent_file.txt"])
+def test_save_to_txt(library: Library, filename: str) -> None:
+    """
+    Тест для метода save_to_txt
+    :param library: Library
+    :param filename: Имя файла str
+    :return: None
+    """
+    person1 = Person("Item 1", 25)
+    person2 = Person("Item 2", 30)
+    library.add_person(person1)
+    library.add_person(person2)
+    library.save_to_txt(filename)
+
+
+@pytest.mark.parametrize("filename", ["test_inventory.txt", "nonexistent_file.txt"])
+def test_load_from_txt(library: Library, filename: str) -> None:
+    """
+    Тест для метода load_from_txt
+    :param library: Library
+    :param filename: имя файла str
+    :return: None
+    """
+    library.load_from_txt(filename)
+
+
+def test_error_handling(library, caplog):
+    """
+    Тест обработки ошибок при добавлении/удалении
+    :param library: Library
+    :param caplog: фиксатор caplog
+    :return: None
+    """
+    with caplog.at_level(logging.WARNING):
+        library.remove_person(Person("Nonexistent Person", 99))
+        assert "Attempt to delete a non-existent person" in caplog.text
+        library.remove_book(Book("Nonexistent Book", "Unknown Author"))
+        assert "Attempt to delete a non-existent book" in caplog.text
+        library.lend_book(Book("Nonexistent Book", "Unknown Author"), Person("Nonexistent Person", 99))
+        assert "It is impossible to issue a book" in caplog.text
