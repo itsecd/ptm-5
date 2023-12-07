8450fd74eaa031bdc2a2627f5732f8e05be8b03d
pavel <tomashaytis@gmail.com>
2023-12-07 21:15:26 +0400

    Добавил тесты для основных функций класса Play.

--- test_play.py
+++ test_play.py
@@ -7,7 +7,7 @@ class TestPlay:
     @classmethod
     def setup_class(cls) -> None:
         """
-        Начальная инициализация полей тестового класса и подмена функций библиотеки curses.
+        Начальная инициализация полей тестового класса.
         :return: нет возвращаемого значения.
         """
         cls.play = Play("#", "*")
@@ -78,3 +78,86 @@ class TestPlay:
         result.append(self.play._pause)
         assert result == [True, False]
 
+    def test_move_snake_head(self):
+        """
+        Функция тестирует перемещение головы змейки по полю в соответствии с её текущим направлением.
+        :return: Нет возвращаемого значения.
+        """
+        result = []
+        screen = FakeScreen()
+        self.play._snake_body = [[10, 10]]
+        self.play._current_direction = 'right'
+        self.play._move_snake_head(screen)
+        result.append(self.play._snake_body[0])
+        self.play._snake_body = [[10, 10]]
+        self.play._current_direction = 'left'
+        self.play._move_snake_head(screen)
+        result.append(self.play._snake_body[0])
+        self.play._snake_body = [[10, 10]]
+        self.play._current_direction = 'up'
+        self.play._move_snake_head(screen)
+        result.append(self.play._snake_body[0])
+        self.play._snake_body = [[10, 10]]
+        self.play._current_direction = 'down'
+        self.play._move_snake_head(screen)
+        result.append(self.play._snake_body[0])
+        assert result == [[10, 11], [10, 9], [9, 10], [11, 10]]
+
+    def test_remove_tail(self):
+        """
+        Функция тестирует удаление хвоста змейки при её движении.
+        :return: Нет возвращаемого значения.
+        """
+        screen = FakeScreen()
+        self.play._snake_body = [[11, 10], [10, 10], [9, 10]]
+        self.play._current_direction = 'bottom'
+        self.play.apple = [50, 50]
+        self.play._remove_the_tail(screen)
+        assert self.play._snake_body == [[11, 10], [10, 10]]
+
+    def test_eat_apple(self):
+        """
+        Функция тестирует сохранение размера змейки при поедании ею яблока.
+        :return: Нет возвращаемого значения.
+        """
+        screen = FakeScreen()
+        self.play.score = [0, 5]
+        self.play._snake_head = [11, 10]
+        self.play._snake_body = [[11, 10], [10, 10], [9, 10]]
+        self.play.apple = [11, 10]
+        self.play._remove_the_tail(screen)
+        assert self.play._snake_body == [[11, 10], [10, 10], [9, 10]] and self.play.score[1] == 6
+
+    def test_run_into_wall(self):
+        """
+        Функция тестирует условие смерти змейки при её столкновении со стеной.
+        :return: Нет возвращаемого значения.
+        """
+        result = []
+        self.play._x_len = 100
+        self.play._y_len = 200
+        self.play._snake_head = [11, 10]
+        self.play._snake_body = [[11, 10]]
+        result.append(self.play._condictions_to_lose())
+        self.play._snake_head = [2, 10]
+        self.play._snake_body = [[2, 10]]
+        result.append(self.play._condictions_to_lose())
+        self.play._snake_head = [197, 10]
+        self.play._snake_body = [[197, 10]]
+        result.append(self.play._condictions_to_lose())
+        self.play._snake_head = [11, 5]
+        self.play._snake_body = [[11, 5]]
+        result.append(self.play._condictions_to_lose())
+        self.play._snake_head = [11, 94]
+        self.play._snake_body = [[11, 94]]
+        result.append(self.play._condictions_to_lose())
+        assert result == [None, 1, 1, 1, 1]
+
+    def test_run_into_itself(self):
+        """
+        Функция тестирует условие смерти змейки при её столкновении с частью своего тела.
+        :return: Нет возвращаемого значения.
+        """
+        self.play._snake_head = [11, 10]
+        self.play._snake_body = [[11, 10], [10, 10], [10, 9], [11, 9], [11, 10]]
+        assert self.play._condictions_to_lose() == 1
