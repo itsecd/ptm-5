df603acfda1ad90bb4530d2f556d254ed2b87096
THO LE LOC <leloctho2307@gmail.com>
2023-12-13 00:11:39 +0400

    исправил db.py and test_unit.py

--- test_unit.py
+++ test_unit.py
@@ -1,6 +1,5 @@
 import pytest
 from db import *
-from main import *
 
 
 @pytest.mark.parametrize("file", ["Employee.db"])
@@ -8,58 +7,48 @@ def test_data_base(file) -> Database:
     return Database(file)
 
 
-@pytest.mark.parametrize("name, age, doj, email, gender, contact, address",
-                         [("Le Loc Tho", "23", "Student", "leloctho@gmail.com", "123456789", "VietNam")])
-def test_insert(name, age, doj, email, gender, contact, address):
-    data = Database('Employee.db')
-    return data.insert(name, age, doj, email, gender, contact, address)
+@pytest.mark.parametrize("file", ["Employee.db"])
+def test_fetch(file):
+    data = Database(file)
+    tmp = data.fetch()
+    assert tmp == [(1, 'John Doe', '30', '2023-01-01', 'john@example.com', 'Male', '1234567890', '123 Main St'),
+                   (2, 'Romie', '23', '2023-07-08', 'romie@gmail.com', 'Male', '1234567890', '759 Main St')]
 
 
-@pytest.mark.parametrize("idx, idx_expect", [('1', '1')])
-def test_remove(idx, idx_expect):
-    data = Database('Employee.db')
-    result = data.remove(idx)
-    assert result == idx_expect
+@pytest.mark.parametrize("file, name, age, doj, email, gender, contact, address",
+                         [("Employee.db", "Daniel", "30", "2000-01-07", "daniel@gmail.com", "Male", "0524567890",
+                           "759 Puma St")])
+def test_insert(file, name, age, doj, email, gender, contact, address):
+    data = Database(file)
+    data.insert(name, age, doj, email, gender, contact, address)
+    assert len(data.fetch()) == 3
 
 
-@pytest.fixture
-def test_getData():
-    """
-        Check that enough data is gotted
-        :return: None
-    """
-    data = Database('Employee.db')
-    data.insert("Le Loc Tho", "23", "Student", "leloctho@gmail.com", "123456789", "VietNam")
-    getData(data)
-    assert name.get() == "Le Loc Tho"
+@pytest.mark.parametrize("file, ind", [("Employee.db", 1)])
+def test_getDataInd(file, ind):
+    data = Database(file)
+    dt = data.get_data_ind(ind)
+    assert dt == (1, 'John Doe', '30', '2023-01-01', 'john@example.com', 'Male', '1234567890', '123 Main St')
 
 
-def test_displayAll():
-    """
-        Check that enough data is displayed on the screen
-        :return: None
-    """
-    data = Database('Employee.db')
-    data.insert("Le Loc Tho", "23", "Student", "leloctho@gmail.com", "123456789", "VietNam")
-    data.insert("David Phuc", "22", "Manager", "phuc@gmail.com", "456789123", "England")
-    display_All()
-    assert len(db.fetch()) == 2
+@pytest.mark.parametrize("file, name", [("Employee.db", "John")])
+def test_FindName(file, name):
+    data = Database(file)
+    data_name = data.find_data(name)
+    assert data_name == True
 
 
-def test_clearAll():
-    """
-        Check if all data has been deleted
-        :return: None
-    """
-    clearAll()
-    assert name.get() == " "
+@pytest.mark.parametrize("file, idx, idx_expect", [("Employee.db", '3', '3')])
+def test_remove(file, idx, idx_expect):
+    data = Database(file)
+    data.remove(idx)
+    tmp = data.fetch()
+    assert tmp == [(1, 'John Doe', '30', '2023-01-01', 'john@example.com', 'Male', '1234567890', '123 Main St'),
+                   (2, 'Romie', '23', '2023-07-08', 'romie@gmail.com', 'Male', '1234567890', '759 Main St')]
 
 
-def test_delete_employee():
-    """
-        Check if a worker's data has been deleted
-        :return: None
-    """
-    data = Database('Employee.db')
-    delete_employee()
-    assert len(data.fetch()) == 1
+@pytest.mark.parametrize("file", ["Employee.db"])
+def test_clearAll(file):
+    data = Database(file)
+    data.clear_ALL()
+    assert len(data.fetch()) == 0
