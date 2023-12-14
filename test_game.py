6032e9b912f47d87e23d69216a7079aed5d20656
MishaIB <mishakrut11@gmail.com>
2023-12-14 21:28:54 +0400

    change unittest  to pytest

--- test_game.py
+++ test_game.py
@@ -1,112 +1,49 @@
-import unittest
-from unittest.mock import patch
-from game import results
+import pytest
+from game import results, user_points, computer_points
 
-class TestGame(unittest.TestCase):
+# Мокируем глобальные переменные
+@pytest.fixture(autouse=True)
+def reset_game_state(monkeypatch):
+    """сброс очков перед каждым тестом
+    """
+    monkeypatch.setattr('game.user_points', 0)
+    monkeypatch.setattr('game.computer_points', 0)
+
+
+@pytest.mark.parametrize("comp_choice, user_input, exp_user_points, exp_comp_points", [
+    ('Snake', 's', 0, 0), 
+    ('Water', 'w', 0, 0),
+    ('Gun', 'g', 0, 0),   
+    ('Snake', 'g', 1, 0),
+    ('Water', 's', 1, 0), 
+    ('Gun', 'w', 1, 0),   
+    ('Snake', 'w', 0, 1), 
+    ('Water', 'g', 0, 1), 
+    ('Gun', 's', 0, 1),  
+])
+def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points, monkeypatch):
+    """тестирование различных сценариев игры и проверка очков
+
+    Args:
+        comp_choice (_type_): выбор компьютера
+        user_input (_type_): выбор пользователя
+        exp_user_points (_type_): ожидаемое количество очков пользователя
+        exp_comp_points (_type_): ожидаемое количество очков компьютера
+    """
+    monkeypatch.setattr('game.computer_choice', comp_choice)
+    monkeypatch.setattr('game.user_choice', user_input)
     
-    def setUp(self):
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        
-    @patch('game.computer_choice', 'Snake')
-    @patch('game.user_choice', 's')
-    def test_result_draw_snake(self):
-        """Проверка, что очки не изменились, если оба выбрали змею
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 0)
-        self.assertEqual(computer_points, 0)
-        
-    @patch('game.computer_choice', 'Water')
-    @patch('game.user_choice', 'w')
-    def test_result_draw_water(self):
-        """Проверка, что очки не изменились, если оба выбрали воду
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 0)
-        self.assertEqual(computer_points, 0)
+    results()
+    assert user_points == exp_user_points
+    assert computer_points == exp_comp_points
 
-    @patch('game.computer_choice', 'Gun')
-    @patch('game.user_choice', 'g')
-    def test_result_draw_gun(self):
-        """Проверка, что очки не изменились, если оба выбрали пистолет
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
+def test_maximum_score(monkeypatch):
+    """проверка на максимальное значение очков
+    """
+    max_rounds = 5
+    monkeypatch.setattr('game.computer_choice', 'Gun')
+    monkeypatch.setattr('game.user_choice', 's')
+    for _ in range(max_rounds):
         results()
-        self.assertEqual(user_points, 0)
-        self.assertEqual(computer_points, 0)
-
-    @patch('game.computer_choice', 'Snake')
-    @patch('game.user_choice', 'w')
-    def test_snake_drinks_water(self):
-        """Проверка, что очки  изменились, если компьютер выбрал змею, а пользователь воду
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 0)
-        self.assertEqual(computer_points, 1)
-
-    @patch('game.computer_choice', 'Water')
-    @patch('game.user_choice', 'g')
-    def test_gun_sinks_in_water(self):
-        """Проверка, что очки  изменились, если компьютер выбрал воду, а пользователь пистолет
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 0)
-        self.assertEqual(computer_points, 1)
-    
-    @patch('game.computer_choice', 'Water')
-    @patch('game.user_choice', 's')
-    def test_snake_beats_water(self):
-        """Проверка, что очки  изменились, если компьютер выбрал воду, а пользователь змею
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 1)
-        self.assertEqual(computer_points, 0)
-
-    @patch('game.computer_choice', 'Snake')
-    @patch('game.user_choice', 'g')
-    def test_gun_beats_snake(self):
-        """Проверка, что очки  изменились, если компьютер выбрал змею, а пользователь пистолет
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 1)
-        self.assertEqual(computer_points, 0)
-
-    @patch('game.computer_choice', 'Gun')
-    @patch('game.user_choice', 'w')
-    def test_water_beats_gun(self):
-        """Проверка, что очки  изменились, если компьютер выбрал оружие, а пользователь воду
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        results()
-        self.assertEqual(user_points, 1)
-        self.assertEqual(computer_points, 0)
-        
-    def test_maximum_score(self):
-        """тест на максимальное количество очков
-        """
-        global user_points, computer_points
-        user_points, computer_points = 0, 0
-        max_rounds = 5
-        for _ in range(max_rounds):
-            with patch('game.computer_choice', 'Gun'), patch('game.user_choice', 's'):
-                results()
-        self.assertEqual(computer_points, max_rounds)
-        self.assertEqual(user_points, 0)
-
-if __name__ == '__main__':
-    unittest.main()
+    assert computer_points == max_rounds
+    assert user_points == 0
