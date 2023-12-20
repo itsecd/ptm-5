3b3423662ac5305cdc7e5cf04585d196b224337b
Миша Чукарев <mishakrut11@gmail.com>
2023-12-20 23:09:09 +0400

    fix

--- test_game.py
+++ test_game.py
@@ -1,6 +1,5 @@
 import pytest
-from game import results, user_points, computer_points
-
+import game
 # Мокируем глобальные переменные
 @pytest.fixture(autouse=True)
 def reset_game_state(monkeypatch):
@@ -15,13 +14,13 @@ def reset_game_state(monkeypatch):
     ('Water', 'w', 0, 0),
     ('Gun', 'g', 0, 0),   
     ('Snake', 'g', 1, 0),
-    ('Water', 's', 1, 0), 
-    ('Gun', 'w', 1, 0),   
-    ('Snake', 'w', 0, 1), 
-    ('Water', 'g', 0, 1), 
-    ('Gun', 's', 0, 1),  
+    ('Water', 's', 2, 0),
+    ('Gun', 'w', 3, 0),
+    ('Snake', 'w', 3, 1),
+    ('Water', 'g', 3, 2),
+    ('Gun', 's', 3, 3),
 ])
-def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points, monkeypatch):
+def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points):
     """тестирование различных сценариев игры и проверка очков
 
     Args:
@@ -30,21 +29,7 @@ def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points,
         exp_user_points (_type_): ожидаемое количество очков пользователя
         exp_comp_points (_type_): ожидаемое количество очков компьютера
     """
-    monkeypatch.setattr('game.computer_choice', comp_choice)
-    monkeypatch.setattr('game.user_choice', user_input)
-    
-    results(comp_choice, user_input)
-    assert user_points == exp_user_points
-    assert computer_points == exp_comp_points
 
-def test_maximum_score(monkeypatch):
-    """проверка на максимальное значение очков
-
-    """
-    max_rounds = 5
-    monkeypatch.setattr('game.computer_choice', 'Gun')
-    monkeypatch.setattr('game.user_choice', 's')
-    for _ in range(max_rounds):
-        results('Gun', 's')
-    assert computer_points == max_rounds
-    assert user_points == 0
+    game.results(comp_choice, user_input)
+    assert game.user_points == exp_user_points
+    assert game.computer_points == exp_comp_points
