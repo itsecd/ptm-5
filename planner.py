d3b81ceba9d21d032e01e85d7d354955c3c9846a
neygenius <mr.neygen@yandex.ru>
2023-12-20 20:44:34 +0400

    + 2 теста и изменил принцип работы

--- planner.py
+++ planner.py
@@ -77,20 +77,21 @@ class Planner:
 
         :param start_date: Дата начала периода
         :param end_date: Дата конца периода
-        :return: None
+        :return: Отчет
         """
-        report = {}
+        report = []
         for date in self.daily_planner:
             if start_date <= date <= end_date:
-                report[date] = self.daily_planner[date]
-        if len(report) > 0:
-            print(f"\nОтчет за период с {start_date} по {end_date}:")
-            for date in report:
-                print(f"{date}:")
-                for task in report[date]:
-                    print(f"- {task}")
+                report.append(date)
+        if len(report) == 0:
+            return f"\nЗа период с {start_date} по {end_date} нет дел\n"
         else:
-            print(f"\nЗа период с {start_date} по {end_date} нет дел")
+            result = f"\nОтчет за период с {start_date} по {end_date}\n\n"
+            for date in report:
+                result += f"{date}:\n"
+                for task in self.daily_planner[date]:
+                    result += f"- {task}\n"
+            return result
 
 
 def main():
@@ -106,7 +107,7 @@ def main():
     todo.view_tasks("2023-12-31")
     todo.view_tasks("2024-01-01")
     todo.view_all_tasks()
-    todo.generate_report("2023-12-31", "2024-01-01")
+    print(todo.generate_report("2023-12-31", "2024-01-01"))
 
 
 if __name__ == "__main__":
--- planner_test.py
+++ planner_test.py
@@ -30,3 +30,35 @@ def test_remove_task():
     planner.add_task("test task", "test time")
     planner.remove_task("test task", "test time")
     assert len(planner.daily_planner) == 0
+
+
+def test_multi_remove_tasks():
+    """
+    Тест на удаление нескольких задач из ежедневника
+    """
+    planner = Planner()
+    planner.add_task("test task 1", "test time 1")
+    planner.add_task("test task 2", "test time 1")
+    planner.add_task("test task 3", "test time 2")
+    planner.add_task("test task 4", "test time 2")
+    planner.add_task("test task 5", "test time 3")
+    planner.remove_task("test task 1", "test time 1")
+    planner.remove_task("test task 2", "test time 1")
+    planner.remove_task("test task 3", "test time 2")
+    planner.remove_task("test task 4", "test time 2")
+    planner.remove_task("test task 5", "test time 3")
+    assert len(planner.daily_planner) == 0
+
+
+@pytest.mark.parametrize("time, expected_result", [
+    ("test time 1", "\nОтчет за период с test time 1 по test time 1\n\n"
+                    "test time 1:\n- test task 1\n- test task 2\n"),
+    ("test time 3", "\nЗа период с test time 3 по test time 3 нет дел\n")
+])
+def test_generate_report(time, expected_result):
+    planner = Planner()
+    planner.add_task("test task 1", "test time 1")
+    planner.add_task("test task 2", "test time 1")
+    assert planner.generate_report(time, time) == expected_result
+
+
