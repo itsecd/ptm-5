9365baef1c778148d0fcf2c8b382de9708f66928
danyala1 <d.mayasov@mail.ru>
2023-12-20 23:57:20 +0400

    Добавил doc-string, pep8, удалил запуск

--- test_unit.py
+++ test_unit.py
@@ -11,36 +11,78 @@ def hero() -> Heroes:
 
 
 def test_change_class(hero: Heroes):
+    """
+    Test for changing the hero's class.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.change_class("Warrior")
     assert hero.hero_class == "Warrior"
 
 
 def test_change_hp(hero: Heroes):
+    """
+    Test for changing the hero's HP.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.change_hp(100)
     assert hero.hp == 100
 
 
 def test_change_damage(hero: Heroes):
+    """
+    Test for changing the hero's damage.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.change_damage(20)
     assert hero.damage == 20
 
 
 def test_change_armor(hero: Heroes):
+    """
+    Test for changing the hero's armor.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.change_armor(50)
     assert hero.armor == 50
 
 
 def test_change_lvl(hero: Heroes):
+    """
+    Test for changing the hero's level.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.change_lvl(5)
     assert hero.lvl == 5
 
 
 def test_hero_exists(hero: Heroes):
+    """
+    Test for checking the existence of a hero with the specified ID.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.add_hero(11, 'HorseMan', 150, 300, 70, 30)
     assert hero.hero_exists(6) == True
@@ -48,6 +90,13 @@ def test_hero_exists(hero: Heroes):
 
 
 def test_can_survive(hero: Heroes):
+    """
+    Test for checking the hero's ability to survive in battle.
+
+    :param hero: The Heroes instance.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.add_hero(11, 'HorseMan', 150, 300, 70, 30)
     assert hero.can_survive(6) == False
@@ -58,11 +107,27 @@ def test_can_survive(hero: Heroes):
 
 @pytest.mark.parametrize("filename", ["test_heroes.csv", "nonexistent_file.csv"])
 def test_read_from_csv(hero: Heroes, filename: str) -> None:
+    """
+    Test for reading hero data from a CSV file.
+
+    :param hero: The Heroes instance.
+    :param filename: The name of the CSV file.
+
+    :return: None
+    """
     hero.read_from_csv(filename)
 
 
 @pytest.mark.parametrize("filename", ["test_heroes.csv", "nonexistent_file.csv"])
 def test_write_to_csv(hero: Heroes, filename: str) -> None:
+    """
+    Test for writing hero data to a CSV file.
+
+    :param hero: The Heroes instance.
+    :param filename: The name of the CSV file.
+
+    :return: None
+    """
     hero.add_hero(6, 'Mage', 80, 30, 5, 7)
     hero.add_hero(12, 'Warlock', 85, 40, 3, 9)
     hero.add_hero(11, 'Shaman', 95, 30, 7, 3)
@@ -70,7 +135,3 @@ def test_write_to_csv(hero: Heroes, filename: str) -> None:
 
     assert os.path.exists(filename)
     assert os.path.getsize(filename) > 0
-
-
-if __name__ == "__main__":
-    pytest.main(["-v", "-color=yes"])
