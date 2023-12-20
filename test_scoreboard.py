850dbd0c0fb23d9ceb4e24bd597527c632e0caaf
diana <diana@DIANA_LAPTOP>
2023-12-20 20:50:39 +0400

    Изменения в test_scoreboard

--- test_scoreboard.py
+++ test_scoreboard.py
@@ -1,35 +1,7 @@
-from unittest.mock import patch, MagicMock
 from Game import ScoreBoard
 
 
-@patch('Game.curses')
-def test_scoreboard_run(mock_curses: MagicMock) -> None:
-    """
-    Тестирует инициализацию и запуск экрана таблицы результатов в классе ScoreBoard.
-    Проверяет, что начальные настройки экрана таблицы результатов были выполнены.
-    """
-    mock_screen = MagicMock()
-    scoreboard = ScoreBoard()
-    scoreboard._ScoreBoard__run(mock_screen)
-    mock_screen.curs_set.assert_called_with(False)
-    mock_screen.clear.assert_called()
-
-
-@patch('Game.curses')
-def test_scoreboard_loop(mock_curses: MagicMock) -> None:
-    """
-    Тестирует игровой цикл таблицы результатов в классе ScoreBoard.
-    Проверяет, что цикл таблицы результатов корректно обрабатывает события и обновляет экран.
-    """
-    mock_screen = MagicMock()
-    scoreboard = ScoreBoard()
-    scoreboard._ScoreBoard__loop(mock_screen)
-    mock_screen.clear.assert_called()
-    mock_screen.refresh.assert_called()
-
-
-@patch('Game.curses')
-def test_scoreboard_add_score(mock_curses: MagicMock) -> None:
+def test_scoreboard_add_score():
     """
     Тестирует добавление нового результата в таблицу результатов.
     Проверяет, что новый результат корректно добавляется в список результатов.
@@ -38,3 +10,25 @@ def test_scoreboard_add_score(mock_curses: MagicMock) -> None:
     test_score = ["2023-12-17", 100]
     scoreboard.add_score(test_score)
     assert test_score in scoreboard._ScoreBoard__score_list, "Новый результат должен быть добавлен в список результатов"
+
+
+def test_scoreboard_sorting_scores():
+    """
+    Тестирует сортировку результатов в таблице результатов.
+    Проверяет, что результаты сортируются в правильном порядке.
+    """
+    scoreboard = ScoreBoard()
+    scores = [["2023-12-17", 50], ["2023-12-18", 100], ["2023-12-16", 75]]
+    for score in scores:
+        scoreboard.add_score(score)
+    sorted_scores = sorted(scores, key=lambda x: x[1], reverse=True)
+    assert scoreboard._ScoreBoard__score_list == sorted_scores, "Результаты должны быть отсортированы по убыванию баллов"
+
+
+def test_scoreboard_initialization():
+    """
+    Тестирует инициализацию объекта ScoreBoard.
+    Проверяет, что все начальные параметры установлены правильно.
+    """
+    scoreboard = ScoreBoard()
+    assert isinstance(scoreboard._ScoreBoard__score_list, list), "Score list должен быть инициализирован как список"
