580701bab9422d9284e21328a6273a82f7c56f66
Миша Чукарев <mishakrut11@gmail.com>
2023-12-20 23:11:32 +0400

    fix

--- test_game.py
+++ test_game.py
@@ -14,13 +14,13 @@ def reset_game_state(monkeypatch):
     ('Water', 'w', 0, 0),
     ('Gun', 'g', 0, 0),   
     ('Snake', 'g', 1, 0),
-    ('Water', 's', 2, 0),
-    ('Gun', 'w', 3, 0),
-    ('Snake', 'w', 3, 1),
-    ('Water', 'g', 3, 2),
-    ('Gun', 's', 3, 3),
+    ('Water', 's', 1, 0),
+    ('Gun', 'w', 1, 0),
+    ('Snake', 'w', 0, 1),
+    ('Water', 'g', 0, 1),
+    ('Gun', 's', 0, 1),
 ])
-def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points):
+def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points, monkeypatch):
     """тестирование различных сценариев игры и проверка очков
 
     Args:
@@ -29,7 +29,8 @@ def test_game_results(comp_choice, user_input, exp_user_points, exp_comp_points)
         exp_user_points (_type_): ожидаемое количество очков пользователя
         exp_comp_points (_type_): ожидаемое количество очков компьютера
     """
-
+    game.computer_points = 0
+    game.user_points = 0
     game.results(comp_choice, user_input)
     assert game.user_points == exp_user_points
     assert game.computer_points == exp_comp_points
