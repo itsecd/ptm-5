1b2624ed2f6dc55e592b27653a479a5f1c50240c
Chuck-man <sgold2398@gmail.com>
2023-12-21 17:45:32 +0400

    Корректировка кода

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from math import sqrt
-from sympy import symbols, simplify, diff, integrate, Eq, solve, Symbol, expand, linsolve
-
+from sympy import symbols, diff, integrate
 
 def square_eq_solver(a, b, c):
    result = []
@@ -8,7 +7,7 @@ def square_eq_solver(a, b, c):
 
    if discriminant == 0:
        result.append(-b / (2 * a))
-   else:
+   elif discriminant > 0:
        result.append((-b + sqrt(discriminant)) / (2 * a))
        result.append((-b - sqrt(discriminant)) / (2 * a))
 
@@ -25,8 +24,9 @@ def derivatives(x, y, expr):
 
     print("Частная производная от выражения по x:", df_dx)
     print("Частная производная от выражения по y:", df_dy)
+    return df_dx, df_dy
 
-def indefinite_integral(x, y, expr):
+def indefinite_integral(x, expr):
     integral_expr = integrate(expr, x)
 
     print("Неопределенный интеграл по x:", integral_expr)
@@ -42,49 +42,45 @@ def commission(a, b, p1, p2):
     c3 = (p1 - p2) * 0.13
     return c1, c2, c3
 
-def profits(p1, p2, c1, c2, c3):
-    p7 = round(sum([c1, c2, c3]))   # сумма затрат
-    p8 = round((p1 - p2) - p7)      # чистая прибыль
-
-    print(f'стоимость акции при покупке - {p2}\n'
-          f'стоимость акции при продаже - {p1}\n'
-          f'чистая прибыль - {p8}\nналоги/комиссии - {p7}')
-
-    return p7, p8
-
 def trapezoid_area(height, base_1, base_2):
     area = ((base_1 + base_2) / 2) * height
-    print('Площадь трапеции: {area}')
+    print('Площадь трапеции: ', area)
     return area
 
 def info():
     print("1. Вычисление корня квадратного уравнения\n"
           "2. Вычисление частной производной\n"
           "3. Вычисление неопределённого интеграла\n"
-          "4. Вычисление стоимости акции\n"
-          "5. Вычисление площади трапеции\n")
+          "4. Вычисление цены\n"
+          "5. Вычисление комиссии\n"
+          "6. Вычисление площади трапеции\n")
 
 
 def main():
     info()
     action = input('Введите номер действия: ')
     match(action):
-        case(1):
+        case('1'):
             a, b, c = map(int, input('Пожалуйста, введите три числа через пробел: ').split())
             result1 = square_eq_solver(a, b, c)
-        case(2):
+        case('2'):
             x, y = symbols('x y')
             expr = input('Введите уравнение: ')
             result2 = derivatives(x, y, expr)
-        case(3):
-            x, y = symbols('x y')
+        case('3'):
+            x = symbols('x')
             expr = input('Введите уравнение: ')
-            result3 = indefinite_integral(x, y, expr)
-        case(4):
+            result3 = indefinite_integral(x, expr)
+        case('4'):
             pricesale = float(input('Введите цену продажи:'))
-            p1, p2 = price(1807.3, pricesale, 6)
+            p1, p2 = price(1807, pricesale, 6)
+            print(p1, p2)
+        case('5'):
+            pricesale = float(input('Введите цену продажи:'))
+            p1, p2 = price(1807, pricesale, 6)
             c1, c2, c3 = commission(0.13, 0.000725, p1, p2)
-        case(5):
+            print(c1, c2, c3)
+        case('6'):
             height = float(input("Height of trapezoid: "))
             base1 = float(input('Base one value: '))
             base1 = float(input('Base two value: '))
