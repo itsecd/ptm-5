7c26f2af63506ad1dd50e7740f252e3d38fe11ef
Chuck-man <sgold2398@gmail.com>
2023-12-22 20:51:14 +0400

    Исправления

--- test_main.py
+++ test_main.py
@@ -4,19 +4,10 @@ from sympy import symbols
 from main import (square_eq_solver, derivatives, indefinite_integral,
                    price, commission, trapezoid_area)
 
-def test_no_root():
-   res = square_eq_solver(10, 0, 2)
-   assert len(res) == 0
-
-def test_single_root():
-   res = square_eq_solver(10, 0, 0)
-   assert len(res) == 1
-   assert res == [0]
-
-def test_multiple_root():
-   res = square_eq_solver(2, 5, -3)
-   assert len(res) == 3
-   assert res == [0.5, -3]
+def test_square():
+    assert square_eq_solver(10, 0, 0) == 0.00
+    assert square_eq_solver(2, 5, -3) == (0.50, -3.00) 
+    assert square_eq_solver(10, 0, 2) == None
 
 def test_derivatives():
     x, y = symbols('x y')
