2e225e10a857133fd08f3b50a89d07a61983a66b
Маргарита <113328619+MaargoGysarova@users.noreply.github.com>
2023-12-24 00:43:21 +0400

    Исправила ошибки все тесты работают

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def test_solve_dice_sum_problem(probability_calculator):
     # Проверяем, что функция solve_dice_sum_problem возвращает ожидаемое значение
     target_sum = 7
     num_dice = 2
-    expected_result = 6/36  # Замените на ожидаемое значение
+    expected_result = 6/36  
     result = probability_calculator.solve_dice_sum_problem(target_sum, num_dice)
     assert result == pytest.approx(expected_result, rel=1e-2)
 
@@ -52,3 +52,47 @@ def test_write_output_file(probability_calculator):
     
     # Удаляем временный файл
     os.remove(test_output_filename)
+
+@pytest.fixture
+def probability_calculator():
+    return ProbabilityCalculator()
+
+# Тесты для функций ProbabilityCalculator
+@pytest.mark.parametrize('dice_number, target_number, expected_probability', [(1, 3, 1/6), (2, 5, 0.16666666666666666)])
+def test_probability_of_dice_roll(probability_calculator, dice_number, target_number, expected_probability):
+    result = probability_calculator.dice_roll_probability.probability_of_dice_roll(dice_number, target_number)
+    assert result == pytest.approx(expected_probability, rel=1e-2)
+
+#pytest test.py
+
+
+@pytest.mark.parametrize('target_sum, expected_probability', [(7, 6/36), (8, 5/36)])
+def test_probability_of_dice_combination(probability_calculator, target_sum, expected_probability):
+    result = probability_calculator.dice_combination_probability.probability_of_dice_combination(target_sum)
+    assert result == pytest.approx(expected_probability, rel=1e-2)
+
+# Создаем стаб-объект ProbabilityCalculator
+class ProbabilityCalculatorStub(ProbabilityCalculator):
+    def probability_of_dice_combination(self, target_sum):
+        if target_sum == 7:
+            return 0.2
+        elif target_sum == 8:
+            return 0.1
+        else:
+            return 0.0
+
+@pytest.fixture
+def probability_calculator_stub():
+    return ProbabilityCalculatorStub() 
+
+def test_probability_of_dice_combination_with_stub(probability_calculator_stub):
+    # Вызываем функцию с использованием стаб-объекта
+    result = probability_calculator_stub.dice_combination_probability.probability_of_dice_combination(7)
+    # Проверяем, что результат равен ожидаемому значению
+    assert result == 0.2
+
+    result = probability_calculator_stub.dice_combination_probability.probability_of_dice_combination(8)
+    assert result == 0.1
+
+    result = probability_calculator_stub.dice_combination_probability.probability_of_dice_combination(9)
+    assert result == 0.0
