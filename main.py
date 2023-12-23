d00f4459b04b31c62c5dbfa1bfe28fda01620964
xwills1337 <misha.sid@rambler.ru>
2023-12-23 05:57:26 +0400

    fix

--- main.py
+++ main.py
@@ -159,8 +159,8 @@ def quadratic_formula(a, b, c):
     discriminant = b**2 - 4*a*c
     if discriminant < 0:
         return "No real roots"
-    root1 = (-b+math.sqrt(discriminant)) / (2*a)
-    root2 = (-b-math.sqrt(discriminant)) / (2*a)
+    root1 = (-b-math.sqrt(discriminant)) / (2*a)
+    root2 = (-b+math.sqrt(discriminant)) / (2*a)
     return root1, root2
 
 
